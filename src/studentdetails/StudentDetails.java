7c09a7a81699bd2168c26b4ca772bac641f625ff
gnhehar <hehgurle@sheridancollege.ca>
2022-05-25 10:04:39 -0400

    version 1 code

---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,32 @@
+package studentdetails;
+
+public class StudentDetails {
+        
+        public static void main(String[] args){
+        
+        
+        Student[] studentlist=new Student[3];
+        
+        Student s1 = new Student();
+        s1.setAge(19);
+        s1.setName("Lilli");
+        
+        Student s2 = new Student();
+        s2.setName("Harman");
+        s2.setAge(19);
+        
+        Student s3 = new Student();
+        s3.setAge(20);
+        s3.setName("sia");
+        
+        studentlist[0]=s1;
+        studentlist[1]=s2;
+        studentlist[2]=s3;
+        
+        for(int i = 0; i<studentlist.length;i++){
+        System.out.println(studentlist[i].getName()+" "+studentlist[i].getAge());
+            }
+        }
+   
+            
+}
